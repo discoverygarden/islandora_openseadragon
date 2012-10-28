0278c3b0449486eee1935c4d0bd0a86956889035
DannyJoris <danny_joris@yahoo.co.uk>
2012-10-28 01:31:32 -0300

    made the module more detectable/pluggable. still in draft.

--- js/islandora_openseadragon.js
+++ js/islandora_openseadragon.js
@@ -1,16 +1,18 @@
 (function($) {
-  Drupal.behaviors.islandora_seadragon = {
+  Drupal.behaviors.islandora_openseadragon = {
     attach: function(context) {
-
+      // get variables
+      var base_path = Drupal.settings.basePath;
+      // islandora_openseadragon variables
       var viewer_id = Drupal.settings.islandora_openseadragon.viewer_id;
       var djatoka_url = Drupal.settings.islandora_openseadragon.djatoka_url;
       var resource_uri = Drupal.settings.islandora_openseadragon.resource_uri;
+      var options = Drupal.settings.islandora_openseadragon.options;
 
-/*       var viewer = new OpenSeadragon.Viewer(viewer_id); */
-
+      // init viewer
       var viewer = new OpenSeadragon({
         id: viewer_id,
-        prefixUrl: "sites/all/libraries/openseadragon",
+        prefixUrl: base_path + "sites/all/libraries/openseadragon",
         showNavigationControl: true/*,
         tileSources: "/openseadragon/examples/images/highsmith/highsmith.js",
         wrapVertical: true,
@@ -23,10 +25,10 @@
 
         */
       }); 
-
+      // get djatoka tilesource
       var djtilesource = new OpenSeadragon.DjTileSource(djatoka_url, resource_uri);
+      // apply djatoka tilesource
       viewer.openTileSource(djtilesource);
-
     }
   };
 })(jQuery);
