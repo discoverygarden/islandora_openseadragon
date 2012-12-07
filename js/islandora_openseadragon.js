e6ad08d94cf3e67ca6e7f0beae3bd8e002744acf
DannyJoris <danny_joris@yahoo.co.uk>
2012-12-07 16:16:13 -0400

    added processed check

--- js/islandora_openseadragon.js
+++ js/islandora_openseadragon.js
@@ -3,13 +3,19 @@
     attach: function(context, settings) {
       var resourceUri = settings.islandoraOpenSeadragon.resourceUri;
       var config = settings.islandoraOpenSeadragon.settings;
-      config.tileSources = new Array();
-      resourceUri = (resourceUri instanceof Array) ? resourceUri : new Array(resourceUri);
-      $.each(resourceUri, function(index, uri) {
-        var tileSource = new OpenSeadragon.DjatokaTileSource(uri, settings.islandoraOpenSeadragon);
-        config.tileSources.push(tileSource);
+      var openSeadragonId = '#' + config['id'];
+      $(openSeadragonId).each(function () {
+          if (!$(this).hasClass('processed')) {
+          config.tileSources = new Array();
+          resourceUri = (resourceUri instanceof Array) ? resourceUri : new Array(resourceUri);
+          $.each(resourceUri, function(index, uri) {
+            var tileSource = new OpenSeadragon.DjatokaTileSource(uri, settings.islandoraOpenSeadragon);
+            config.tileSources.push(tileSource);
+          });
+          new OpenSeadragon(config);
+          $(this).addClass('processed');
+        }
       });
-      new OpenSeadragon(config);
     }
   };
 })(jQuery);
