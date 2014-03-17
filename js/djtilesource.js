f1bcf72c6843b5e8de40e74704009f27f227481b
Stefan Langer <slanger@worthingtonlibraries.org>
2014-03-17 12:43:43 -0400

    ISLANDORA-27 Updated regex, escaping backslashes instead.

--- js/djtilesource.js
+++ js/djtilesource.js
@@ -27,9 +27,9 @@
       // Determine if the path to "imagefile" has backslashes in it.
       if (data.match(/\"imagefile\"\: \"[^\"]*?(\\).*\"/g)) {
         // Backslashes in the JP2 file path (Windows) will get mistaken for escaped
-        // characters. Replace them with forwardslashes so the viewer doesn't break.
+        // characters. Escape the backslashes themselves so that viewer doesn't break.
         data = data.replace(/(\"imagefile\"\: \")(.*?)(\")/g, function (match, capture1, imagepath, capture3) {
-          return capture1 + imagepath.replace(/\\/g, '/') + capture3
+          return capture1 + imagepath.replace(/\\/g, '\\\\') + capture3
         });
       }
       data = jQuery.parseJSON(data);
